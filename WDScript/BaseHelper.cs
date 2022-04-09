f24661c4bfde8df375a3158e506d42911f240887
Dynesshely <catrol@qq.com>
2022-04-10 05:12:34 +0800

    💾 Feat: Basic argument process

---
+++ WDScript/BaseHelper.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WDScript
+{
+    internal class BaseHelper
+    {
+        /// <summary>
+        /// 指定长度字符构造字符串
+        /// </summary>
+        /// <param name="length">长度</param>
+        /// <param name="sign">字符</param>
+        /// <returns>构造好的字符串</returns>
+        internal static string BuildString(int length, char sign)
+        {
+            string str = "";
+            for (int i = 0; i < length; i++) str += sign;
+            return str;
+        }
+
+        /// <summary>
+        /// 控制台彩色打印
+        /// </summary>
+        /// <param name="content">打印内容</param>
+        /// <param name="cc">颜色</param>
+        internal static void Write(string content, ConsoleColor cc)
+        {
+            ConsoleColor ccn = Console.ForegroundColor;
+            Console.ForegroundColor = cc;
+            Console.Write(content);
+            Console.ForegroundColor = ccn;
+        }
+
+        /// <summary>
+        /// 控制台彩色打印追加换行
+        /// </summary>
+        /// <param name="content">打印内容</param>
+        /// <param name="cc">颜色</param>
+        internal static void WriteLine(string content, ConsoleColor cc)
+        {
+            ConsoleColor ccn = Console.ForegroundColor;
+            Console.ForegroundColor = cc;
+            Console.WriteLine(content);
+            Console.ForegroundColor = ccn;
+        }
+    }
+}
