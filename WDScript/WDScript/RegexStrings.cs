1f895b6f5386edbfdcd96cccd34fca32835d0436
Dynesshely <catrol@qq.com>
2022-04-10 16:52:29 +0800

    📦 Struct: Create RegexStrings

---
+++ WDScript/WDScript/RegexStrings.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WDScript.WDScript
+{
+    internal static class RegexStrings
+    {
+        internal const string regex_expression = "";
+    }
+}
