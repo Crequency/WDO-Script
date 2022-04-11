2528f661267d8f8edb8c932267bc4dedff18c1fc
Dynesshely <catrol@qq.com>
2022-04-11 23:11:21 +0800

    💾 Feat: Exception Type

---
+++ WDScript/WDScript/Exceptions/Exception.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WDScript.WDScript.Exceptions
+{
+    public class Exception
+    {
+        public bool IsHandled { get; set; }
+
+        public string Message { get; set; }
+
+        public string Position { get; set; }
+
+        public string FileName { get; set; }
+    }
+}
